fc916243cf7551b221d0308cb26362099128aa3e
jzl <zhanling_jin@163.com>
2024-12-19 16:40:46 +0800

    兼容蓝走运老版本

--- client.go
+++ client.go
@@ -113,7 +113,22 @@ func (c *Client) DownInfo(fileId int64) (*DownInfoResp, error) {
 	if shareInfoResp.Zt != 1 {
 		return nil, fmt.Errorf("获取分享连接失败%v", shareInfoResp)
 	}
-
+	if strings.Contains(shareInfoResp.Info.IsNewd, "lanzouu.com") {
+		uuu, err := getLanRealDown(fmt.Sprintf("%s/%s", shareInfoResp.Info.IsNewd, shareInfoResp.Info.FId))
+		if err != nil {
+			return nil, err
+		}
+		uv, err := url.Parse(uuu)
+		if err != nil {
+			return nil, err
+		}
+		return &DownInfoResp{
+			Zt:  1,
+			Dom: fmt.Sprintf("%s://%s", uv.Scheme, uv.Host),
+			Url: uv.RequestURI(),
+			Inf: "",
+		}, nil
+	}
 	// 获取分享页面
 	body, err := c.getDownPage(fmt.Sprintf("%s/%s", shareInfoResp.Info.IsNewd, shareInfoResp.Info.FId))
 	if err != nil {
@@ -269,6 +284,65 @@ func (c *Client) UploadFile(filepath string, dirId int64) (*UploadFileResp, erro
 		return nil, errFile10
 	}
 	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", c.domain+"/html5up.php", payload)
+	if err != nil {
+		return nil, err
+	}
+	c.fillHeader(req)
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := c.htpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	return jsonUnmarshal[UploadFileResp](body)
+}
+func (c *Client) UploadFileFromReader(file io.Reader, fileName string, dirId int64) (*UploadFileResp, error) {
+	ext := strings.TrimLeft(path.Ext(fileName), ".")
+	mime, ok := mimeMap[ext]
+	if !ok {
+		return nil, errors.New("不允许的文件格式")
+	}
+
+	// 格式化布局
+	layout := "Mon Jan 2 2006 15:04:05 GMT-0700"
+
+	// 格式化时间，不包括时区详细名称
+	formattedTime := time.Now().Format(layout)
+
+	// 手动添加时区名称
+	chineseTimeZone := "中国标准时间"
+
+	// 输出格式化后的时间，带有时区名称
+	modTime := fmt.Sprintf("%s (%s)\n", formattedTime, chineseTimeZone)
+
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	_ = writer.WriteField("task", "1")
+	_ = writer.WriteField("vie", "2")
+	_ = writer.WriteField("ve", "2")
+	_ = writer.WriteField("id", "WU_FILE_0")
+	_ = writer.WriteField("name", path.Base(fileName))
+	_ = writer.WriteField("type", mime)
+	_ = writer.WriteField("lastModifiedDate", modTime)
+	_ = writer.WriteField("size", "218025")
+	_ = writer.WriteField("folder_id_bb_n", strconv.FormatInt(dirId, 10))
+
+	part10, errFile10 := writer.CreateFormFile("upload_file", path.Base(fileName))
+	_, errFile10 = io.Copy(part10, file)
+	if errFile10 != nil {
+		return nil, errFile10
+	}
+	err := writer.Close()
 	if err != nil {
 		return nil, err
 	}
@@ -570,4 +644,183 @@ func (c *Client) DownInfoBySign(fileId int64, skdklds string, pwd string) (*Down
 		return nil, err
 	}
 	return jsonUnmarshal[DownInfoResp](body)
+}
+func getLanRealDownFromBody(domain string, respBody []byte) (string, error) {
+
+	if len(respBody) == 0 {
+		return "", errors.New("body长度为0")
+	}
+	//log.Println(string(respBody))
+
+	srcReg, err := regexp.Compile(`iframe.*src="(.*?)".*?iframe`)
+	if err != nil {
+		return "", err
+	}
+	list := srcReg.FindStringSubmatch(string(respBody))
+	if len(list) != 2 {
+		return "", errors.New("没有匹配地址")
+	}
+
+	//log.Println("网盘下载页", fmt.Sprintf("%s/%s", domain, list[1]))
+
+	downResp, err := http.Get(fmt.Sprintf("%s/%s", domain, list[1]))
+	if err != nil {
+		return "", err
+	}
+	defer downResp.Body.Close()
+	downBody, err := io.ReadAll(downResp.Body)
+	if err != nil {
+		return "", err
+	}
+	//log.Println(string(downBody))
+
+	uReg, err := regexp.Compile(`(/ajaxm.php.*?)'`)
+	if err != nil {
+		return "", err
+	}
+	list = uReg.FindStringSubmatch(string(downBody))
+	if len(list) != 2 {
+		return "", errors.New("未查到ajaxm")
+	}
+	ajaxUrl := list[1]
+	dataReg, err := regexp.Compile(`data.*?:(.*?\})`)
+	if err != nil {
+		return "", err
+	}
+	list = dataReg.FindStringSubmatch(string(downBody))
+	if len(list) != 2 {
+		return "", errors.New("未查到ajaxm参数")
+	}
+	ajaxBody := list[1]
+	ajaxBody = strings.ReplaceAll(ajaxBody, "ajaxdata", `'?ctdf'`)
+	ajaxBody = strings.ReplaceAll(ajaxBody, "ciucjdsdc", `''`)
+	ajaxBody = strings.ReplaceAll(ajaxBody, "aihidcms", `'7Sij'`)
+	ajaxBody = strings.ReplaceAll(ajaxBody, "kdns", `1`)
+	ajaxBody = strings.ReplaceAll(ajaxBody, `'`, `"`)
+	//log.Println(ajaxUrl, ajaxBody)
+	return downAjax(domain, ajaxUrl, ajaxBody)
+
+}
+func getLanRealDown(pageUrl string) (string, error) {
+	method := "GET"
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, pageUrl, nil)
+
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7")
+	req.Header.Add("accept-language", "zh-CN,zh;q=0.9")
+	req.Header.Add("cache-control", "no-cache")
+	//req.Header.Add("cookie", "codelen=1; Hm_lvt_fb7e760e987871d56396999d288238a4=1734582425; HMACCOUNT=232C742A38316425; uz_distinctid=193dd2b584bc5a-00b9cc741d1317-26011851-130980-193dd2b584c10a6; STDATA82=czst_eid%3D62426063-3821-%26ntime%3D3821; pc_ad1=1; Hm_lpvt_fb7e760e987871d56396999d288238a4=1734595884; codelen=1; pc_ad1=1")
+	req.Header.Add("pragma", "no-cache")
+	req.Header.Add("priority", "u=0, i")
+	req.Header.Add("sec-ch-ua", "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Add("sec-fetch-dest", "document")
+	req.Header.Add("sec-fetch-mode", "navigate")
+	req.Header.Add("sec-fetch-site", "none")
+	req.Header.Add("sec-fetch-user", "?1")
+	req.Header.Add("upgrade-insecure-requests", "1")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	respBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	if len(respBody) == 0 {
+		return "", fmt.Errorf("body长度为0 %d", resp.StatusCode)
+	}
+	uu, err := url.Parse(pageUrl)
+	if err != nil {
+		return "", err
+	}
+	return getLanRealDownFromBody(fmt.Sprintf("%s://%s", uu.Scheme, uu.Host), respBody)
+}
+func downAjax(domain, path string, data string) (string, error) {
+	uuu := domain + path
+	method := "POST"
+
+	m := make(map[string]interface{})
+	err := json.Unmarshal([]byte(data), &m)
+	if err != nil {
+		return "", err
+	}
+	uv := url.Values{}
+	for k, v := range m {
+		switch vv := v.(type) {
+		case int64:
+			uv.Add(k, strconv.FormatInt(vv, 10))
+		case string:
+			uv.Add(k, vv)
+		case float64:
+			uv.Add(k, strconv.FormatInt(int64(vv), 10))
+		}
+	}
+	client := &http.Client{}
+	//log.Println(uv.Encode())
+	req, err := http.NewRequest(method, uuu, strings.NewReader(uv.Encode()))
+
+	if err != nil {
+		return "", err
+	}
+	req.Header.Add("accept", "application/json, text/javascript, */*")
+	req.Header.Add("accept-language", "zh-CN,zh;q=0.9")
+	req.Header.Add("cache-control", "no-cache")
+	req.Header.Add("content-type", "application/x-www-form-urlencoded")
+	//req.Header.Add("cookie", "codelen=1; Hm_lvt_fb7e760e987871d56396999d288238a4=1734582425; HMACCOUNT=232C742A38316425; uz_distinctid=193dd2b584bc5a-00b9cc741d1317-26011851-130980-193dd2b584c10a6; STDATA82=czst_eid%3D62426063-3821-%26ntime%3D3821; pc_ad1=1; Hm_lpvt_fb7e760e987871d56396999d288238a4=1734596612; codelen=1; pc_ad1=1")
+	req.Header.Add("origin", "https://wwrq.lanzouu.com")
+	req.Header.Add("pragma", "no-cache")
+	req.Header.Add("priority", "u=1, i")
+	req.Header.Add("referer", "https://wwrq.lanzouu.com/fn?UjRbMVk2VDFVN1A0AmJSYFc7UGxWPgsvBHcEP1M_bUWdXZFQ3XDQHalMyVzNRNAY1US0FdwM5VjYFcQZpBT5VOlI5W2lZfVQ9VUZQMQI0UiM_c")
+	req.Header.Add("sec-ch-ua", "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"")
+	req.Header.Add("sec-ch-ua-mobile", "?0")
+	req.Header.Add("sec-ch-ua-platform", "\"Windows\"")
+	req.Header.Add("sec-fetch-dest", "empty")
+	req.Header.Add("sec-fetch-mode", "cors")
+	req.Header.Add("sec-fetch-site", "same-origin")
+	req.Header.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+	req.Header.Add("x-requested-with", "XMLHttpRequest")
+
+	//log.Println("请求ajax", req)
+	res, err := client.Do(req)
+	//fmt.Println("ajax相应", res, err)
+	if err != nil {
+		return "", err
+	}
+
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return "", errors.New(res.Status)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	vvv := DownAjaxResp{}
+	err = json.Unmarshal(body, &vvv)
+	if err != nil {
+		return "", err
+	}
+	if vvv.Zt == 0 {
+		return "", fmt.Errorf("%d", vvv.Zt)
+	}
+	//https://down-load.lanrar.com/file/?BWMBP1tqUmMIAQE5U2ZdMVplVGwFvQaMVuAH5Fe7UOAC5QbZW7cPvwPvUYECuQK/UoBTsFGEApYH6QafVLtWsAWGAftb4FKrCNIBuFO7XdVaLVS3BdAGkla4B55XuFC2AoEG/FvgD/YD2VEpAjACb1JpUzVRKAJmB2kGbFRtVggFbAEyWztSPghnAWRTMl1qWjNUaQViBiJWNwdyVzZQYgIxBmhbNg9vA2FRNAJnAiVSeFMmUTMCMgcwBjJUOlZ4BTQBZ1spUjcIZgF4Uz9dPlpjVDcFYgYwVmMHM1dtUGUCOwYwWzMPbAMwUTICYgI3UjFTZVFoAjAHNgZkVGlWYwVkAWVbMVI1CDsBYlMpXTpabFQwBTkGIlYkB3JXblAjAmsGNVs7D2MDY1E1AmECM1I9U3BRegJpB20GZVRuVmoFNAFhWzVSNwhqAW9TMV1kWjNUYAV0BipWdwdnV2dQJgI/BmBbMQ9oA2RRMwJuAjdSP1NuUTsCJgd1BnBUf1ZqBTQBYFswUj4IaQFnUzVdbFo3VGYFfAZxVjgHcVc2UGACMgZlWygPagNjUT8CeAI2UjFTeFE9AjUHLgYmVGxWOAVyAThbWVJlCDUBalM3
+	//"<a href="+dom_down+"/file/"+ date.url + lanosso +" target=_blank rel=noreferrer//><span class=txt>电信下载</span><span class='txt txtc'>联通下载</span><span class=txt>普通下载</span></a>
+	return fmt.Sprintf("%s/%s", vvv.Dom, vvv.Url), nil
+}
+
+type DownAjaxResp struct {
+	Zt  int    `json:"zt"`
+	Dom string `json:"dom"`
+	Url string `json:"url"`
+	Inf int    `json:"inf"`
 }
